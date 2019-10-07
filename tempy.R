f179fc53fd52c5df85ade1071b6efd2ab5058310
cwoll920 <cwoll920@uwsp.edu>
2019-10-07 17:38:37 -0500

    Commit message number 2

---
+++ tempy.R
@@ -0,0 +1,7 @@
+WVSData <- read.csv(file="C:\\Users\\cwoll920\\OneDrive - UWSP\\DAC Seminar\\WV_US.csv")
+summary(WVSData$v23)
+str(WVSData)
+myReg=lm(V23~V59,WVSData)
+summary(myReg)
+summary(WVSData$V23)
+hist(WVSData$V23)
